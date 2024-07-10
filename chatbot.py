19cb135ce53c0f33978bb6936835b4d0b12443f6
Colby <colbysmith99@hotmail.com>
2024-07-10 17:38:50 +0100

    Update-3
    Change logging structure, added more response data

--- chatbot.py
+++ chatbot.py
@@ -2,12 +2,14 @@ import re
 import nltk
 from nltk.tokenize import word_tokenize
 from chatbot_data import responses
+import logging
+import os
+
 nltk.download('punkt')
+logging.basicConfig(filename='chat_log.txt', level=logging.INFO, format='%(asctime)s - %(message)s')
 
 def log_interaction(user_input, bot_response):
-    with open('chat_log.txt', 'a') as log_file:
-        log_file.write(f"User: {user_input}\n")
-        log_file.write(f"Bot: {bot_response}\n\n")
+    logging.info(f"User: {user_input}\nBot: {bot_response}")
 
 def get_response(user_input):
     user_input = user_input.lower()
@@ -17,13 +19,13 @@ def get_response(user_input):
         if any(re.search(pattern, token) for token in tokens):
             return response
     
-    return f"{responses.get('default', 'Sorry, I don\'t understand that question.')} {suggest_questions()}"
+    return f"{responses.get('default', 'Sorry, I do not understand that question.')} {suggest_questions()}"
 
 def suggest_questions():
     return "Here are some basic questions you can ask me: 'What are your hobbies?', 'What is your educational background?', Also you can respond with 'help' for a list of all keywords I am programmed to answer."
 
 def main():
-    print("Hello! My name is Colby, an artifical construct made by Colby to be as close to the real thing as possible!")
+    print("Hello! My name is Colby, an artificial construct made by Colby to be as close to the real thing as possible!")
     print(suggest_questions())
     while True:
         user_input = input("You: ")
